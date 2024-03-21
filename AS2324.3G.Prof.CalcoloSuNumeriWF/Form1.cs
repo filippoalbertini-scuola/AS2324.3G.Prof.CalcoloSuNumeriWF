7434d3c73a9d1b731d7c6edf6ef8a5bb16fa4472
Filippo Albertini <filippo@filippoalbertini.it>
2024-03-21 08:41:29 +0100

    feat: implementazione somma, conteggio e media con colorazione in rosso per numero di numeri maggiore o uguale a 5

--- AS2324.3G.Prof.CalcoloSuNumeriWF/Form1.cs
+++ AS2324.3G.Prof.CalcoloSuNumeriWF/Form1.cs
@@ -2,6 +2,10 @@ namespace AS2324._3G.Prof.CalcoloSuNumeriWF
 {
     public partial class Form1 : Form
     {
+        // definizione della variabile per contenere il numero di numeri
+        int nNumeri = 0;
+        double somma = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -9,10 +13,27 @@ namespace AS2324._3G.Prof.CalcoloSuNumeriWF
 
         private void btnAggiungi_Click(object sender, EventArgs e)
         {
+
+            // incremento il numero di numeri inseriti
+            nNumeri += 1;
+
             double numero = Convert.ToDouble(txtNumero.Text);
 
+            // accumulo per la somma
+            somma += numero;
 
+            // calcolo media
+            double media = somma/(double)nNumeri;
 
+            // visualizzazione informazioni
+            lblConteggio.Text = "Conteggio " + nNumeri.ToString();
+            lblSomma.Text = "Somma " + somma.ToString();
+            lblMedia.Text = $"Media {media}";
+
+            if (nNumeri >= 5)
+                lblConteggio.ForeColor = Color.Red;
+            else
+                lblConteggio.ForeColor = Color.Black;
         }
     }
 }
