ef335f398fd9be740781645b14bb1a394560b7d8
Filippo Albertini <filippo@filippoalbertini.it>
2024-03-21 08:21:12 +0100

    feat: definizione della UI dell'applicativo

--- AS2324.3G.Prof.CalcoloSuNumeriWF/Form1.Designer.cs
+++ AS2324.3G.Prof.CalcoloSuNumeriWF/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnAggiungi = new Button();
+            txtNumero = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // btnAggiungi
+            // 
+            btnAggiungi.Location = new Point(356, 65);
+            btnAggiungi.Name = "btnAggiungi";
+            btnAggiungi.Size = new Size(107, 40);
+            btnAggiungi.TabIndex = 0;
+            btnAggiungi.Text = "Add";
+            btnAggiungi.UseVisualStyleBackColor = true;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(225, 72);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(125, 27);
+            txtNumero.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(156, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Numero";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(225, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Somma :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(225, 186);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Conteggio :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(225, 225);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 2;
+            label4.Text = "Media :";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(699, 305);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtNumero);
+            Controls.Add(btnAggiungi);
+            Name = "Form1";
+            Text = "Prof;3G;21/03/24;Raccolta di numeri con calcolo di somma, conteggio e media";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnAggiungi;
+        private TextBox txtNumero;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
